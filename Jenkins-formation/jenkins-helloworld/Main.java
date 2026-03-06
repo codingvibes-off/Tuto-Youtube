6e6024202cec7a10823d2131f3c51fb31929cad4
Sullivan Sextius <215255114+codingvibes-off@users.noreply.github.com>
2026-03-06 09:51:38 +0100

    + push project test academy
    + push project test academy

--- Jenkins-formation/jenkins-helloworld/Main.java
+++ Jenkins-formation/jenkins-helloworld/Main.java
@@ -3,3 +3,24 @@ public class Main {
         System.out.println("Hello world");
     }
 }
+
+pipeline {
+agent any
+stages {
+stage ('clone') {
+steps {
+sh
+"rm -rf *"
+sh "git clone https://github.com/priximmo/jenkins-helloworld"
+｝
+}
+stage ('build') {
+steps {
+sh "cd jenkins-helloworld/ && javac Main.java"
+｝
+}
+stage('run') {
+steps {
+sh "cd jenkins-helloworld/ && java Main"
+}
+｝
